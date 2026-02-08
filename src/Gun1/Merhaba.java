07e0095907e6745914f4f420f9707cb34687f5e6
hatun <hatunyuksel.106@gmail.com>
2026-02-08 14:07:01 +0300

    Kredi kartı kontrolü eklendi

--- src/Gun1/Merhaba.java
+++ src/Gun1/Merhaba.java
@@ -4,5 +4,6 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("merhaba 1");
         System.out.println("merhaba 2");
+        System.out.println("merhaba 3");
     }
 }
