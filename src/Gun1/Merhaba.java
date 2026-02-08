5c0ae1190ce10d2308574e70150a96a4cb470146
hatun <hatunyuksel.106@gmail.com>
2026-02-08 14:09:37 +0300

    Satın alma işkemi onayı eklendi

--- src/Gun1/Merhaba.java
+++ src/Gun1/Merhaba.java
@@ -4,6 +4,9 @@ public class Merhaba {
     public static void main(String[] args) {
         System.out.println("merhaba 1");
         System.out.println("merhaba 2");
-        System.out.println("merhaba 3");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba");
+
+        }
     }
 }
