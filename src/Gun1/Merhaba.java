b6503b4e428a273505468cac18cbcb889e44a6bf
hatun <hatunyuksel.106@gmail.com>
2026-02-08 14:11:00 +0300

    task3

--- src/Gun1/Merhaba.java
+++ src/Gun1/Merhaba.java
@@ -3,7 +3,10 @@ package Gun1;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("merhaba 1");
-        System.out.println("merhaba 2");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("merhaba 5");
+
+        }
         for (int i = 0; i < 5; i++) {
             System.out.println("merhaba");
 
